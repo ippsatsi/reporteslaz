1ff425ea9246a2e2b5045430855ac02664e2e0a7
ippsatsi <laz133@gmail.com>
2018-01-15 07:10:18 -0500

    Empezando borrr_gestiones... - Mejorando el formulario

--- func_procesos.php
+++ func_procesos.php
@@ -2,18 +2,28 @@
 
 function mostrar_gestiones () {
 ?>
+      <form action="borrargestiones.php" method="post">
+        <fieldset>
+          <legend>Borrado de Gestiones</legend>
+          <label for="dni"> Buscar DNI: </label>
+          <input id="dni" type="text" name="dni"> 
 <table>
   <thead>
     <tr>
       <th>Marcar</th>
+      <th>Cuenta</th>
       <th>Observaciones</th>
       <th>Respuesta</th>
       <th>Solucion</th>
       <th>Fecha Gestion</th>
+      <th>Status</th>
     </tr>
   </thead>
   <tr>
     <td><input type="checkbox" name="check_list[]" value="3025">
+    </td>
+    <td>
+      001101234567891231
     </td>
     <td>
       NO CONTESTA
@@ -26,8 +36,13 @@ function mostrar_gestiones () {
     <td>
       05/01/2018 10:55:25.236
     </td>
+    <td>
+      visible
+    </td>
   </tr>
 </table>
+        </fieldset>
+      </form>
 <?php
 }
 ?>
