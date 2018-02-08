d34b5676f923c404bc04bbfbbeaa27da7edf2cc5
ippsatsi <laz133@gmail.com>
2018-02-08 06:32:53 -0500

    Completando los archivos del progresivo

---
+++ querys_borrargestiones.php
@@ -0,0 +1,105 @@
+<?php
+
+function buscar_dni($dni) {
+    $conn = conectar_mssql();
+  $query = "
+SELECT
+CUE.CUE_NROCUENTA
+, SCA.SCA_DESCRIPCION
+, ISNULL(TMO.DIC_SIMBOLO + ' ','') + CAST(BDE.BAD_DEUDA_SALDO AS VARCHAR) AS BAD_DEUDA_SALDO
+, ISNULL(TMO.DIC_SIMBOLO + ' ','') + CAST(BDE.BAD_DEUDA_MONTO_CAPITAL AS VARCHAR) AS BAD_DEUDA_MONTO_CAPITAL
+FROM
+COBRANZA.GCC_CUENTAS CUE
+INNER JOIN COBRANZA.GCC_CLIENTE CLI ON CLI.CLI_CODIGO=CUE.CLI_CODIGO
+INNER JOIN COBRANZA.GCC_BASEDET BDE ON BDE.CUE_CODIGO=CUE.CUE_CODIGO AND BDE.BAD_ESTADO_CUENTA='A'
+INNER JOIN COBRANZA.GCC_BASE BAS ON BAS.BAS_CODIGO=BDE.BAS_CODIGO
+INNER JOIN COBRANZA.GCC_SUBCARTERAS SCA ON SCA.SCA_CODIGO=BAS.SCA_CODIGO
+LEFT JOIN COBRANZA.GCC_DICCIONARIO TMO ON TMO.DIC_CODIGO =CUE.MON_CODIGO AND TMO.DIC_GRUPO=1
+WHERE
+CLI.CLI_DOCUMENTO_IDENTIDAD='".$dni."';";
+  
+  $result_query = sqlsrv_query( $conn, $query, PARAMS_MSSQL_QUERY, OPTIONS_MSSQL_QUERY );
+    if (!$result_query) {
+    throw new Exception('No se pudo completar la consulta por documento',3);
+  }
+  if(sqlsrv_num_rows($result_query)>0){
+    $array_query_result= array();
+    while( $row = sqlsrv_fetch_array($result_query, SQLSRV_FETCH_NUMERIC) ) {
+      $array_query_result[] = $row;
+    }
+    sqlsrv_free_stmt($result_query);
+    return $array_query_result;
+  }else {
+    sqlsrv_free_stmt($result_query);
+    return false;
+  }
+}
+
+function buscar_gestiones($dni, $usuario) {
+    $conn = conectar_mssql();
+  $query = "
+SELECT
+CASE
+   WHEN GES.GES_ESTADO_REGISTRO='I' OR RTAB.CUOTAS=1 OR DATEDIFF(MINUTE,GES.GES_FECHA_REGISTRO,GETDATE())>10
+       THEN CONCAT('<input type=\"checkbox\" name=\"check_list[]\" value=\"',GES.GES_CODIGO,'\" disabled>')
+   ELSE CONCAT('<input type=\"checkbox\" name=\"check_list[]\" value=\"',GES.GES_CODIGO,'\">') END AS CODIGO
+, CUE.CUE_NROCUENTA
+, GES.GES_OBSERVACIONES
+, TRES.TIR_DESCRIPCION
+, ISNULL(TSOL.TIR_DESCRIPCION, '') AS SOLUCION
+, CONVERT(VARCHAR,GES.GES_FECHA_REGISTRO, 103) + ' '+GES.GES_HORA AS FECHA
+, CASE
+   WHEN GES.GES_ESTADO_REGISTRO='A' AND RTAB.CUOTAS=1 THEN 'CONVENIO'
+   WHEN GES.GES_ESTADO_REGISTRO='A' AND RTAB.CUOTAS IS NULL THEN 'VISIBLE'
+   ELSE 'BORRADA' END AS STATUS
+FROM
+COBRANZA.GCC_CUENTAS CUE
+INNER JOIN COBRANZA.GCC_CLIENTE CLI ON CLI.CLI_CODIGO=CUE.CLI_CODIGO
+INNER JOIN COBRANZA.GCC_GESTIONES GES ON GES.CUE_CODIGO=CUE.CUE_CODIGO
+INNER JOIN COBRANZA.GCC_TIPO_RESULTADO TRES ON TRES.TIR_CODIGO=GES.TIR_CODIGO
+LEFT JOIN COBRANZA.GCC_TIPO_RESULTADO TSOL ON TSOL.TIR_CODIGO=GES.SOL_CODIGO
+INNER JOIN COBRANZA.GCC_USUARIO USU ON USU.USU_CODIGO=GES.USU_CODIGO
+LEFT JOIN COBRANZA.GCC_TIPO_RESULTADO_TAB RTAB ON RTAB.TAB_CODIGO=GES.SOL_CODIGO
+WHERE
+CLI.CLI_DOCUMENTO_IDENTIDAD='".$dni."'
+AND GES.USU_CODIGO=".$usuario."
+AND CONVERT(VARCHAR,GES.GES_FECHA,103)=CONVERT(VARCHAR,GETDATE(),103)
+ORDER BY 1 DESC;";
+  
+  $result_query = sqlsrv_query( $conn, $query, PARAMS_MSSQL_QUERY, OPTIONS_MSSQL_QUERY );
+    if (!$result_query) {
+    throw new Exception('No se pudo completar la consulta de gestiones',4);
+  }
+  if(sqlsrv_num_rows($result_query)>0){
+    $array_query_result= array();
+    while( $row = sqlsrv_fetch_array($result_query, SQLSRV_FETCH_NUMERIC) ) {
+      $array_query_result[] = $row;
+    }
+    sqlsrv_free_stmt($result_query);
+    return $array_query_result;
+  }else {
+    sqlsrv_free_stmt($result_query);
+    return false;
+  }
+}
+
+function borrar_gestiones($str_gestiones) {
+  $conn = conectar_mssql();
+  $query = "
+  UPDATE GES
+  SET
+  GES.GES_ESTADO_REGISTRO='I'
+  , GES.TXMOTIVOBAJA='IP:".$_SERVER['REMOTE_ADDR']."'
+  , FEBAJA=GETDATE()
+  FROM
+  COBRANZA.GCC_GESTIONES GES
+  WHERE
+  GES.GES_CODIGO IN (".$str_gestiones.")";
+  
+  $result_query = sqlsrv_query( $conn, $query, PARAMS_MSSQL_QUERY, OPTIONS_MSSQL_QUERY );
+  if (!$result_query) {
+    throw new Exception('No se pudo completar la consulta de borrado de gestiones',5);
+  }
+  return true;
+}
+?>
---
+++ querys_call.php
@@ -0,0 +1,70 @@
+<?php
+function reporte_1($cartera, $fecha_desde, $fecha_hasta) {
+//reporte telefonos progresivo
+  ini_set('sqlsrv.ClientBufferMaxKBSize','524288');
+  $conn = conectar_mssql();
+  $query = "
+  SELECT
+REPLACE(TEL.TEL_NUMERO, CHAR(9), '') TELEFONO
+, CCUE.CUE_NROCUENTA
+, CCUE.CUE_CODIGO
+, TEL.TEL_CODIGO
+, CCUE.CLI_DOCUMENTO_IDENTIDAD
+, ORI.TOR_DESCRIPCION
+, TEL.TEL_ESTADO_VALIDEZ
+, TEL.TEL_OBSERVACIONES
+, SCA.SCA_DESCRIPCION
+FROM
+COBRANZA.GCC_TELEFONOS TEL
+INNER JOIN (
+SELECT
+CUE.CLI_CODIGO
+, CUE.CUE_CODIGO
+, CUE.CUE_NROCUENTA
+, CLI.CLI_DOCUMENTO_IDENTIDAD
+, BDE.BAD_DEUDA_SALDO
+, BAS.SCA_CODIGO
+, ROW_NUMBER() OVER(PARTITION BY CLI.CLI_DOCUMENTO_IDENTIDAD ORDER BY BDE.BAD_DEUDA_SALDO DESC) AS ORDEN
+FROM
+COBRANZA.GCC_CUENTAS CUE
+INNER JOIN COBRANZA.GCC_CLIENTE CLI ON CLI.CLI_CODIGO=CUE.CLI_CODIGO
+INNER JOIN COBRANZA.GCC_BASEDET BDE ON BDE.CUE_CODIGO=CUE.CUE_CODIGO AND BDE.BAD_ESTADO_CUENTA='A'
+INNER JOIN COBRANZA.GCC_BASE BAS ON BAS.BAS_CODIGO=BDE.BAS_CODIGO AND BAS.CAR_CODIGO=".$cartera."
+) CCUE ON CCUE.CLI_CODIGO=TEL.CLI_CODIGO AND CCUE.ORDEN=1
+INNER JOIN COBRANZA.GCC_SUBCARTERAS SCA ON SCA.SCA_CODIGO=CCUE.SCA_CODIGO
+LEFT JOIN COBRANZA.GCC_TIPO_ORIGEN ORI ON ORI.TOR_CODIGO=TEL.TOR_CODIGO
+WHERE
+TEL.TEL_ESTADO_REGISTRO='A'
+ORDER BY 5 ASC, 2 DESC";
+  
+  $result_query = sqlsrv_query( $conn, $query, PARAMS_MSSQL_QUERY, OPTIONS_MSSQL_QUERY );
+//  if (!$result_query) {
+//    throw new Exception('No se pudo completar la consulta11',11);
+   // echo "6";
+  //}
+  if( ($errors = sqlsrv_errors() ) != null) {
+    foreach( $errors as $error ) {
+      echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+      echo "code: ".$error[ 'code']."<br />";
+      echo "message: ".$error[ 'message']."<br />";
+    }
+    echo $query;
+    print_r( $errors, false);
+    exit;
+  }
+
+  $array_query_result = array();
+  $header = array();
+  foreach(sqlsrv_field_metadata($result_query) as $meta) {
+    $header[] = $meta['Name'];
+  }
+  $array_query_result[] = $header;
+  //print_r($header);
+  while( $row = sqlsrv_fetch_array($result_query, SQLSRV_FETCH_NUMERIC) ) {
+    $array_query_result[] = $row;
+  }
+ // print_r($array_query_result);
+  sqlsrv_free_stmt($result_query);
+  return $array_query_result;
+}
+?>
